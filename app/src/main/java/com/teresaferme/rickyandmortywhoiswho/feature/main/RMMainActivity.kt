ac6cc9ff3daf9738922e175fe41911a2b799fe5a
Teresa Fernández <teresaferme@gmail.com>
2023-10-04 12:50:56 +0200

    Added title

--- app/src/main/java/com/teresaferme/rickyandmortywhoiswho/feature/main/RMMainActivity.kt
+++ app/src/main/java/com/teresaferme/rickyandmortywhoiswho/feature/main/RMMainActivity.kt
@@ -3,14 +3,21 @@ package com.teresaferme.rickyandmortywhoiswho.feature.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.Observer
 import com.teresaferme.rickyandmortywhoiswho.model.RMCharacter
@@ -46,9 +53,19 @@ class RMMainActivity : ComponentActivity() {
 
     @Composable
     private fun Content() {
-        CharacterList(
-            characterList = this.characterList.value
-        )
+        Column {
+            Text(
+                modifier = Modifier
+                    .padding(20.dp)
+                    .fillMaxWidth(),
+                text = "Characters",
+                fontSize = 30.sp,
+                textAlign = TextAlign.Center
+            )
+            CharacterList(
+                characterList = characterList.value
+            )
+        }
     }
 
     @Preview(showBackground = true)
